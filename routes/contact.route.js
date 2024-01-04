65cbad7bb46c86e39c61458d041826f7e6fb0acf
RajuNiranjan <rajuniranjan1910@gmail.com>
2024-01-04 13:12:18 +0530

    created controllers

--- routes/contact.route.js
+++ routes/contact.route.js
@@ -1,18 +1,15 @@
 const express = require("express");
+const {
+  getContact,
+  createContact,
+  updateContact,
+  deleteContact,
+} = require("../controller/contact.controller");
 
 const router = express.Router();
 
-router.get("/", (req, res) => {
-  res.send("get all contacts");
-});
-router.post("/", (req, res) => {
-  res.send("create contact");
-});
-router.put("/:id", (req, res) => {
-  res.send(`update contact ${req.params.id}`);
-});
-router.delete("/:id", (req, res) => {
-  res.send(`delete contact for ${req.params.id}`);
-});
+router.get("/", getContact);
+router.post("/", createContact);
+router.put("/:id", updateContact).delete("/:id", deleteContact);
 
 module.exports = router;
